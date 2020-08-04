6c5fa7a6611f82d37b021228906af7ea1c07a44f
tvanhatalo <43368401+tvanhatalo@users.noreply.github.com>
2020-08-04 14:53:11 +0200

    Everything works when running without labview

--- labview_infer.py
+++ labview_infer.py
@@ -1,21 +1,24 @@
 import numpy as np
-from tensorflow import keras
+import tensorflow as tf
 
 array_2D = np.zeros((200,300))
 
 def super_resolve(array_2D):
     # Change model input shape to accept all size inputs
-    model = tf.keras.models.load_model('C://Users//Tara//Desktop//generator_x2.h5', compile=False)
+    model = tf.keras.models.load_model('C:/Users/Tara/Desktop/generator_x2.h5', compile=False)
     inputs = tf.keras.Input((None, None, 3))
     output = model(inputs)
-    model = keras.models.Model(inputs, output)
+    model = tf.keras.models.Model(inputs, output)
         
     # Convert 2D_array to 3-channel np.array
-    low_res = np.asarray(array_2D)
-    low_res = np.repeat(low_res, 3, axis=2)
+    low_res = np.zeros( ( np.asarray(array_2D).shape[0], np.asarray(array_2D).shape[1], 3 ) )
+    low_res[:,:,0] = array_2D # same value in each channel
+    low_res[:,:,1] = array_2D
+    low_res[:,:,2] = array_2D
 
     # Rescale to 0-1.
     low_res = low_res / 255.0
+    print(low_res.shape) # it's the right shape
 
     # Get super resolution image
     sr = model.predict(np.expand_dims(low_res, axis=0))[0]
@@ -24,9 +27,9 @@ def super_resolve(array_2D):
     sr = ((sr + 1) / 2.) * 255
 
     # Return to 1-channel
-    sr_2D = sr[0]
+    sr_2D = sr[:,:,0]
 
     return sr_2D
 
 sr = super_resolve(array_2D)
-print(sr)
+print(sr.shape)
